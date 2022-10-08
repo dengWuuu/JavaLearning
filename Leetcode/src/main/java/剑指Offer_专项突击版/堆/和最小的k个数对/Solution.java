1d83e49a05e79b03adeec626507a9bdda4b656a9
Jackson_Wu <dengwu.wu@foxmail.com>
2022-10-08 19:20:09 +0800

    字节跳动题库

---
+++ Leetcode/src/main/java/剑指Offer_专项突击版/堆/和最小的k个数对/Solution.java
@@ -0,0 +1,35 @@
+package 剑指Offer_专项突击版.堆.和最小的k个数对;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @author Wu
+ * @date 2022年10月07日 23:13
+ */
+public class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(k, (o1, o2)->{
+            return nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]];
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        int m = nums1.length;
+        int n = nums2.length;
+        for (int i = 0; i < Math.min(m, k); i++) {
+            pq.offer(new int[]{i,0});
+        }
+        while (k-- > 0 && !pq.isEmpty()) {
+            int[] idxPair = pq.poll();
+            List<Integer> list = new ArrayList<>();
+            list.add(nums1[idxPair[0]]);
+            list.add(nums2[idxPair[1]]);
+            ans.add(list);
+            if (idxPair[1] + 1 < n) {
+                pq.offer(new int[]{idxPair[0], idxPair[1] + 1});
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ Leetcode/src/main/java/字节跳动题库/两数之和II输入有序数组/Solution.java
@@ -0,0 +1,11 @@
+package 字节跳动题库.两数之和II输入有序数组;
+
+/**
+ * @author Wu
+ * @date 2022年10月08日 19:19
+ */
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+    }
+}
