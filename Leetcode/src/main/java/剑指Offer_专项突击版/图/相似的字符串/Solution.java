eeef9f3da03545c1559f2bd75d8fcbd09f3a4787
Jackson_Wu <dengwu.wu@foxmail.com>
2022-10-22 12:00:42 +0800

    剑指offer2完结

---
+++ Leetcode/src/main/java/剑指Offer_专项突击版/图/相似的字符串/Solution.java
@@ -0,0 +1,53 @@
+package 剑指Offer_专项突击版.图.相似的字符串;
+
+/**
+ * @author Wu
+ * @date 2022年10月22日 10:21
+ */
+public class Solution {
+    int[] f;
+
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        f = new int[n];
+        for (int i = 0; i < n; i++) {
+            f[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int fi = find(i), fj = find(j);
+                if (fi == fj) {
+                    continue;
+                }
+                if (check(strs[i], strs[j], m)) {
+                    f[fi] = fj;
+                }
+            }
+        }
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (f[i] == i) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+
+    public int find(int x) {
+        return f[x] == x ? x : (f[x] = find(f[x]));
+    }
+
+    public boolean check(String a, String b, int len) {
+        int num = 0;
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                num++;
+                if (num > 2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
