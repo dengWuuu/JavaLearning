548a5c181952ec279d8a2f2f1481e1a5c5e4d821
Wu <dengwu.wu@foxmail.com>
2022-11-24 14:51:21 +0800

    每日一题

--- Leetcode/src/main/java/腾讯题库/格雷编码/Solution.java
+++ Leetcode/src/main/java/腾讯题库/格雷编码/Solution.java
@@ -1,9 +1,21 @@
 package 腾讯题库.格雷编码;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    public List<Integer> grayCode(int n) {
+    List<Integer> ans = new ArrayList<>();
 
+    public List<Integer> grayCode(int n) {
+        ans.add(0);
+        int head = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = ans.size() - 1; j >= 0; j--) {
+                ans.add(head + ans.get(i));
+            }
+            head <<= 1;
+        }
+
+        return ans;
     }
 }
